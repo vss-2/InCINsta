3ed0904594134157bfdb1a58d2555dac31d41f41
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-08-10 11:43:30 -0300

    Adicionadas telas de perguntas 1 e 2

---
+++ TelaPerg2.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Button , Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Constants } from 'expo';
+
+
+import AssetExample from './components/AssetExample';
+
+
+import { Card } from 'react-native-elements';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+      <View style = {styles.header}>
+      <Text style = {styles.header}>Blocados</Text>
+      </View>
+      <View style = {styles.enunciado}>
+      <Text style = {styles.enunciado}>  Qual alternativa não é uma estrutura de repetição: </Text>
+      <Text style = {styles.enunciado}>   int soma1 = 5; </Text>
+      <Text style = {styles.enunciado}>   int soma2 = 7; </Text>
+      <Text style = {styles.enunciado}>   int total = 0; </Text>
+      <Text style = {styles.enunciado}>  for (int i = 0; i < soma2; i++) </Text>
+      <Text style = {styles.enunciado}>   total += soma1; </Text>
+      <Text style = {styles.enunciado}>   } </Text>
+      <Text style = {styles.enunciado}>    System.out.println(total); </Text>
+      <Text style = {styles.enunciado}>   } </Text>
+</View>
+        <ScrollView contentContainerStyle = {styles.repoList}>
+          <Button title="Alternativa A" color="#4f4a4a"/>
+          <Text style={styles.repoP1}>For </Text>
+          <Button title="Alternativa B" color="#4f4a4a"/>
+          <Text style={styles.repoP1}>While </Text>
+          <Button title="Alternativa C" color="#4f4a4a"/>
+          <Text style={styles.repoP1}>Do-While </Text>
+          <Button title="Alternativa D" color="#4f4a4a"/>
+          <TouchableOpacity style = {styles.button}
+                onPress = { () => this.props.navigation.navigate("TelaPerg2")}>
+                    <Text style={styles.repoP1}>Switch</Text>
+            </TouchableOpacity>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#2289A9',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  repoP1 :{
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    height: 60,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    borderRadius: 5,
+  },
+  header :{
+    height: 80,
+    paddingTop: 5,
+    backgroundColor: '#2289A9',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  enunciado :{
+    height: 50,
+    paddingTop: 5,
+    textAlign: 'center',
+    backgroundColor: '#2289A9',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
